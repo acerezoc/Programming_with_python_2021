6fdcda3ac0059f17844585a6eddbb348bab7cec3
acerezoc <alvarocc05@hotmail.com>
2020-11-15 00:44:27 +0100

    Added my_city

---
+++ acerezoc/my_city.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+##################################################
+# This script shows an example of a header section. This sections is a group of commented lines (lines starting with
+# the "#" character) that describes general features of the script such as the type of licence (defined by the
+# developer), authors, credits, versions among others
+##################################################
+#
+##################################################
+# Author: Alvaro Cerezo
+# Copyright: Copyright 2020, IAAC
+# Credits: [Institute for Advanced Architecture of Catalonia - IAAC, Advanced Architecture group]
+# License:  Apache License Version 2.0
+# Version: 1.0.0
+# Maintainer: Michael DiCarlo
+# Email: MichaelrDiCarlo@gmail.com
+# Status: development
+##################################################
+
+# End of header section
+
+city_name = "Madrid"
+city_population = 4000000
+city_province = "Madrid"
+city_area = 1000000
+
+print("Population density: " + str(city_population))
